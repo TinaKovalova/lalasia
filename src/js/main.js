6ee3070253b7e89ff2bc3cbc945f3ec7cfbbfe83
TinaKovalova <kovalevatkv@gmail.com>
2025-06-01 21:03:48 +0300

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
 import "../scss/style.scss";
+window.addEventListener("load", () => {
+
+    const navigation = document.querySelector(".navigation");
+    const actions = document.querySelector(".actions");
+    const menuBurger = document.querySelector('.menu-burger');
+    const burgerBtn = document.querySelector(".burger-btn ");
+
+    const matchMedia = window.matchMedia("(max-width: 991.98px)");
+    if (matchMedia.matches && menuBurger) {
+        if (actions) {
+            menuBurger.insertAdjacentElement("beforeend", actions);
+        } 
+        if (navigation) {
+            menuBurger.insertAdjacentElement("beforeend", navigation);
+        }
+    }
+    burgerBtn?.addEventListener("click", () => {
+        console.log(menuBurger);
+        menuBurger?.classList.toggle("_active");
+    })
+    
+});
