75eb2e856b38cf187d0ae90ea55802ba6b657832
TinaKovalova <kovalevatkv@gmail.com>
2025-06-08 17:38:09 +0300

    header adaptive value

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,26 @@
 import "/src/scss/style.scss";
+
 window.addEventListener("load", () => {
 
     const navigation = document.querySelector(".navigation");
     const actions = document.querySelector(".actions");
     const menuBurger = document.querySelector('.menu-burger');
     const burgerBtn = document.querySelector(".burger-btn ");
+    const header = document.querySelector(".header");
+    
+    document.documentElement.style.setProperty(
+      "--scroll-margin-top",
+      `${header.clientHeight}px`
+    );
 
-    const matchMedia = window.matchMedia("(max-width: 991.98px)");
-    if (matchMedia.matches && menuBurger) {
-        if (actions) {
-            menuBurger.insertAdjacentElement("beforeend", actions);
-        } 
-        if (navigation) {
-            menuBurger.insertAdjacentElement("beforeend", navigation);
-        }
-    }
-    burgerBtn?.addEventListener("click", () => {
+    header.addEventListener("chenge", (e) => {
+        
+    })
+    burgerBtn?.addEventListener("click", (e) => {
         console.log(menuBurger);
+        e.target.classList.toggle("_active");
         menuBurger?.classList.toggle("_active");
     })
     
 });
+
